be962ebc5733df43ae34e0d6852123b9557c3730
Karl Schmaltz <karlschmaltz@gmail.com>
2021-10-27 06:10:55 -0600

    fix: missing platforms and gitignore issue

--- src/algolia/algolia.d.ts
+++ src/algolia/algolia.d.ts
@@ -1,5 +1,10 @@
+export declare class Algolia {
+    constructor(appID: string, apiKey: string);
+    initIndex(name: string): AlgoliaIndex;
+}
+
 export declare class AlgoliaIndex {
-    constructor(client: Client, name: string);
+    constructor(client: any, name: string);
     search(query: string, args?: any[]): Promise<void>;
     setSettings(settings: any): Promise<void>;
     saveObjects(objects: any[]): Promise<void>;
